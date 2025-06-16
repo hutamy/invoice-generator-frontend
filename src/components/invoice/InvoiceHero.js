b601aee18d7bdbbbfeaac93ef6b44a8cf5c44d98
hutamy <hutamy.triesthi@dto.kemkes.go.id>
2025-06-16 11:47:24 +0700

    chore: update hero copy

--- src/components/invoice/InvoiceHero.js
+++ src/components/invoice/InvoiceHero.js
@@ -4,7 +4,7 @@ export default function Example() {
       <div className="mx-auto max-w-2xl py-16 sm:py-36">
         <div className="text-center">
           <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-            Effortless Billing: Your Ultimate Invoice Generator
+            Effortless Billing: Free Invoice Generator
           </h1>
         </div>
       </div>
